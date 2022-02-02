de61685b0b88ca18031710162f9fbbcc5d9b7116
Benjamin <benjamin.corre@gmail.com>
2022-02-02 12:20:52 +0100

    refactor(httpstatus): use pattern matching

--- bookingcar-infrastructure/src/main/java/bco/bookingcar/infrastructure/primary/error/status/AppHttpStatusErrorGetter.java
+++ bookingcar-infrastructure/src/main/java/bco/bookingcar/infrastructure/primary/error/status/AppHttpStatusErrorGetter.java
@@ -1,5 +1,7 @@
 package bco.bookingcar.infrastructure.primary.error.status;
 
+import bco.bookingcar.application.car.CarNotFoundException;
+import bco.bookingcar.application.customer.CustomerNotFoundException;
 import bco.bookingcar.exceptions.BusinessException;
 import bco.bookingcar.exceptions.TechnicalException;
 import org.springframework.http.HttpStatus;
@@ -10,14 +12,11 @@ public class AppHttpStatusErrorGetter implements HttpStatusErrorGetter {
     @Override
     public HttpStatus getStatut(BusinessException exception) {
         if (exception == null) return HttpStatus.BAD_REQUEST;
-        //TODO use pattern matching
-        switch (exception.getClass().getSimpleName()) {
-            case "CarNotFoundException":
-            case "CustomerNotFoundException":
-                return HttpStatus.NOT_FOUND;
-            default:
-                return HttpStatus.BAD_REQUEST;
-        }
+        return switch (exception) {
+            case CarNotFoundException carNotFoundException -> HttpStatus.NOT_FOUND;
+            case CustomerNotFoundException customerNotFoundException -> HttpStatus.NOT_FOUND;
+            default -> HttpStatus.BAD_REQUEST;
+        };
     }
 
     @Override
